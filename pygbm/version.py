3488255a87254c40a76b57d5a9eb372b9899111b
randa1029 <randahejiarong@icloud.com>
2025-11-10 02:32:25 +0000

    Update version.py

--- pygbm/version.py
+++ pygbm/version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev12+gd231caf91.d20251110'
-__version_tuple__ = version_tuple = (0, 1, 'dev12', 'gd231caf91.d20251110')
+__version__ = version = '0.1.dev13+g4c0bb3841.d20251110'
+__version_tuple__ = version_tuple = (0, 1, 'dev13', 'g4c0bb3841.d20251110')
 
-__commit_id__ = commit_id = 'gd231caf91'
+__commit_id__ = commit_id = 'g4c0bb3841'
