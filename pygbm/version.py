0691ea36b142fa811e189166f84049ed58003949
randa1029 <randahejiarong@icloud.com>
2025-11-10 02:56:59 +0000

    change package name so can upload to PyPi

--- pygbm/version.py
+++ pygbm/version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.1.dev0+g3488255a8.d20251110'
-__version_tuple__ = version_tuple = (0, 1, 1, 'dev0', 'g3488255a8.d20251110')
+__version__ = version = '0.1.0.post1'
+__version_tuple__ = version_tuple = (0, 1, 0, 'post1')
 
-__commit_id__ = commit_id = 'g3488255a8'
+__commit_id__ = commit_id = 'g5f1989dea'
